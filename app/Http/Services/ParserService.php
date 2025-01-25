3c1dce668c247df48b0cc2832f4b9a72b4e3048e
Vitalii Riabyshenko <wibston1234@gmail.com>
2025-01-25 16:54:41 +0200

    add olx parse pattens

--- app/Http/Services/ParserService.php
+++ app/Http/Services/ParserService.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Services;
 
-use App\Contracts\Parser;
-use DOMDocument;
 use DOMXPath;
+use DOMDocument;
+use App\Contracts\Parser;
+use Illuminate\Support\Str;
 
 class ParserService implements Parser
 {
     public function parser($url)
     {
+		$parse_url = parse_url($url);
+
+		$patern = '//*[@id="mainContent"]/div[2]/main/section/div[1]/div/div[5]/div[1]/div[1]/div[2]/p[2]';
+		if (isset($parse_url['path']) && $parse_url['path'] && Str::of($parse_url['path'])->startsWith("/d/")) {
+			$patern = '//*[@id="mainContent"]/div/div[2]/div[3]/div[2]/div[1]/div/div[3]/div/div/h3';
+		}
+
         $user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36';
 		$timeout = 120;
 
@@ -46,7 +54,7 @@ class ParserService implements Parser
 
         $xpath = new DOMXPath($document);
 
-        $node = $xpath->query('//*[@id="mainContent"]/div/div[2]/div[3]/div[2]/div[1]/div/div[3]/div/div/h3', $document)->item(0);
+        $node = $xpath->query($patern, $document)->item(0);
 
         return $node?->textContent;
     }
